ccdf48f7105ce20276b1c9bb9345a2e43dd3b909
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-13 01:41:48 +0530

    Time: 3 ms (87.72%) | Memory: 20.6 MB (5.20%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        tot = sum(nums)
+        left = 0
+
+        for idx, val in enumerate(nums):
+            if left == (tot - left - val):
+                return idx
+            left += val
+        return -1
