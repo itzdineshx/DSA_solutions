1f9d8b255e3cec4542c6773c3bcf94f784bf2311
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-13 04:48:11 +0530

    Time: 2 ms (41.12%) | Memory: 19.4 MB (5.66%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,13 +1,33 @@
 class Solution(object):
     def minTimeToVisitAllPoints(self, points):
         """
-        :type points: List[List[int]]
-        :rtype: int
+        Algorithm:
+        1. Start from the last point in the list(x1, y1).
+        2. Repeatedly move to the next point(x2, y2).
+        3. For each move from (x1, y1) to (x2, y2):
+           - The minimum time required is: max(|x2 - x1|, |y2 - y1|)
+        4. Accumulate and Return the total time.
         """
-        res = 0
-        x1, y1 = points.pop()
-        while points:
-            x2, y2 = points.pop()
-            res += max(abs(x2-x1),abs(y2-y1))
-            x1, y1 = x2, y2
-        return res
+        # Appraoch 1: modifying points
+        # min_time = 0
+        # x1, y1 = points.pop()
+        # while points:
+        #     x2, y2 = points.pop()
+        #     min_time += max(abs(x2-x1),abs(y2-y1))
+        #     x1, y1 = x2, y2
+        # return min_time
+
+        # Appraoch 2: without modifying points
+        min_time = 0
+        n = len(points)
+        for i in range(1,n):
+            x1, y1 = points[i-1]
+            x2, y2 = points[i]
+            min_time += max(abs(x2-x1),abs(y2-y1))
+        return min_time
+
+"""
+Complexity:
+Time : O(n) - Traversal through points
+Space : O(1) - Constant
+"""
