c71afeff6f22ae4a7fbab936b99e7038f2747cca
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-14 00:57:47 +0530

    Time: 8 ms (25.70%) | Memory: 20.6 MB (7.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,17 +1,30 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        i = 0
-        while i < n:
-            if nums[i] == 0:
-                nums.pop(i)
-                nums.append(0)
-                n -= 1
-            else:
-                i += 1
+#         #1. Brute Force Approach
+#         n = len(nums)
+#         i = 0
+#         while i < n:
+#             if nums[i] == 0:
+#                 nums.pop(i)
+#                 nums.append(0)
+#                 n -= 1
+#             else:
+#                 i += 1
+# """
+# Complexity:
+# Time : O(n^2) - Pop shifts element
+# Space: O(1)
+# """
+            # 2. Two pointer Approach
+            n = len(nums)
+            j = 0
+            for i in range(n):
+                if nums[i] != 0: # if 0 finds
+                    nums[j], nums[i] = nums[i], nums[j] # swap
+                    j += 1
+                else:
+                    pass
+
 """
 Complexity:
 Time : O(n) - Traversal
