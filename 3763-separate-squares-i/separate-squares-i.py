9075575fc52e13e51e3cb836f1527853d5a42c62
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-16 02:56:09 +0530

    Time: 1622 ms (77.80%) | Memory: 48.3 MB (84.58%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -1,5 +1,16 @@
 class Solution:
     def separateSquares(self, squares: List[List[int]]) -> float:
+        """
+        Algorithm:
+        ----------
+        1. Compute the total area of all squares.
+        2. Set low to the as lower bound of y and high as the upper bound of y.
+        3. While high - low is greater than precised val(10^-5):
+            - Take mid = (low + high) / 2 and calc areas
+            - If area below ≥ total area -> high = mid
+            - Else move low = mid
+        4. Return low.
+        """
         total_area = sum(l * l for _, _, l in squares) # Total area of all squares
 
         # Binary search bounds
@@ -17,19 +28,18 @@ class Solution:
                     area += l * (H - y)
             return area
 
-        # Binary search for balance point
-        eps = 1e-6
+        # Balance point
+        eps = 1e-6 # avoid overflow
         while high - low > eps:
             mid = (low + high) / 2
             if area_below(mid) >= total_area / 2:
                 high = mid
             else:
                 low = mid
-
         return low
 
 """
 Complexity:
-Time: O(n log r)
-space: O(1)
+Time: O(n log r) - no of squares x area
+space: O(1) - Constant
 """
